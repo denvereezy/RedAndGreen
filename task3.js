1fe11979107e12ea2fe1d54fac2602790d6eca91
denver <denver@projectcodex.co>
2015-02-18 14:56:04 +0200

    task 3.js

---
+++ task3.js
@@ -0,0 +1,6 @@
+ function myFunction() {
+document.getElementById("whitesquare").style.backgroundColor = "red";
+}
+function myFunction2() {
+document.getElementById("whitesquare").style.backgroundColor = "#00FF00";
+}
