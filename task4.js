a4a0c8cd53814e34cd9c5a4b755deb899a06ca3e
denver <denver@projectcodex.co>
2015-02-23 10:59:03 +0200

    task 4.js

---
+++ task4.js
@@ -0,0 +1,20 @@
+var currentColor = "green"
+
+function myFunction(){
+   if(currentColor === "green"){
+
+       currentColor = "red";
+       document.getElementById("square").style.backgroundColor = "red";
+       document.getElementById("button1").value = "Make green";
+    
+   }
+   else{ 
+   	  currentColor = "green";
+   	   document.getElementById("square").style.backgroundColor = "green";
+       document.getElementById("button1").value = "Make red";
+
+   }
+ 
+
+};
+ 
