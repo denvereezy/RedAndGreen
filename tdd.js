7682fdbb97cef2ddd760a40d9d3bb0c9a13ac476
denver <denver@projectcodex.co>
2015-03-02 09:42:36 +0200

    task 7

---
+++ tdd.js
@@ -0,0 +1,32 @@
+var passes = 0;
+var fails = 0;
+
+var assert = {
+
+	equals:function (expectedResult, returnedResult) {
+		// body...
+		var myDiv = new RedOrGreen("div");
+
+		if (expectedResult === returnedResult) {
+			myDiv.makeGreen();
+			++passes;
+		}
+		else {
+
+			myDiv.makeRed();
+			++fails;
+		}
+	var stats = passes+fails;
+	var newP = document.createElement("p");
+	newP.innerHTML = "Passes: " + passes + " Fails: " + fails + " Total tests: " + stats;
+	document.body.appendChild(newP);
+	}
+};
+
+var TestMyCode = {
+	run:function(nameOfTest, functionToExecute){
+		this.testToRun = nameOfTest;
+		functionToExecute(assert);
+	}
+};
+
